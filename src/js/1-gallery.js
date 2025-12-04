aab8b63c9525460b5ab063de7d0ea8ca84eb6c2f
Riky-Droyg <riky.droyg@gmail.com>
2025-12-04 18:23:58 +0200

    main

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import "/css/styles.css"
+
 
 const images = [
   {
--- src/main.js
+++
@@ -1,3 +0,0 @@
-import './js/1-gallery'
-import './js/2-form'
-import './css/styles.css'
